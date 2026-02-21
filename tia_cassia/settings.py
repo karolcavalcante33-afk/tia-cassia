b8337c95773a79c40dcfd647c3ce3f0c7456a84b
karolcavalcante33-afk <karolcavalcante33@gmail.com>
2026-02-21 10:27:30 -0300

    Configura produção Render - DEBUG False

--- tia_cassia/settings.py
+++ tia_cassia/settings.py
@@ -7,9 +7,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-trocar-essa-chave'
 
 # Modo de depuração ativado para desenvolvimento
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["tia-cassia.onrender.com"]
 
 # Aplicativos instalados
 INSTALLED_APPS = [
